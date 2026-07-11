f39efe27710e737a69f4b86678c13dd9fa714cfd
khoa.hdinh <khoa.hdinh@gmail.com>
2026-07-11 10:34:33 +0700

    Added code files for entire book.

--- part01_basics/chapter02_variables/favorite_number.py
+++ part01_basics/chapter02_variables/favorite_number.py
@@ -1,3 +1,5 @@
 favotite_number = 13;
 
 print("My favorite number is: " + str(favotite_number))
+# Lucien
+# print my favorite number by convert favotite_number (int) to string type then using print() menthod 

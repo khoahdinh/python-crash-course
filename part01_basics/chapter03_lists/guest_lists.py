9efd59242925e558527d640cab6504e6c5089ce3
khoa.hdinh <khoa.hdinh@gmail.com>
2026-07-15 11:31:32 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter03_lists/guest_lists.py
@@ -0,0 +1,19 @@
+guest_lists = ["Dung Dung", "BongBang", "Thao Dung"]
+print(guest_lists)
+
+invite_message = "Welcome you to my dinner, Mr. " + guest_lists[0] + "!"
+print(invite_message)
+
+print("I'm afraid Mr." + guest_lists[1] + " can't make it tonight!")
+
+guest_lists[1] = "Laurence"
+print("So I will call Mr." + guest_lists[1] + " for instead hehe")
+print(guest_lists)
+
+print("My gosh, there is so much love in me so I thinking about bring more folks to the table")
+guest_lists.insert(0, 'Lovelace')
+guest_lists.insert(2, 'Bro')
+guest_lists.append('Khoa')
+print(guest_lists)
+
+print("Hello Everyone")

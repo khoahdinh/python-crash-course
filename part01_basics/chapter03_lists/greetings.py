b2ae5af00f8baedb25cda7d03db13561bb2e77f7
khoa.hdinh <khoa.hdinh@gmail.com>
2026-07-15 09:55:01 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter03_lists/greetings.py
@@ -0,0 +1,13 @@
+names = ["Laurence", "Hai", "Hoang", "Luci"]
+
+laurence_message = "Yo, " + names[0] +  "!"
+hai_message = "What sup, " + names[1] +  "?"
+hoang_message = "You borrowed me 47k, " + names[2] +  ". Where is my money?"
+luci_message = "Great life, " + names[3]
+
+
+
+print(laurence_message)
+print(hai_message)
+print(hoang_message)
+print(luci_message)

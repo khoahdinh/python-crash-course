cdabceaf1dc4e88c285745658c74800e3ad045ec
khoa.hdinh <khoa.hdinh@gmail.com>
2026-07-15 20:58:58 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter03_lists/seeing_the_worlds.py
@@ -0,0 +1,31 @@
+
+locations = ["Japan", "American", "France", "England", "Swedish"]
+print("Five places in the world I want to visit:")
+print(locations)
+
+print("Sorted List:")
+print(sorted(locations))
+
+print("Original List:")
+print(locations)
+
+print("Reverse sorted List:")
+print(sorted(locations, reverse= True))
+
+
+print("Reverse List:")
+locations.reverse()
+print(locations)
+
+print("Reverse List again:")
+locations.reverse()
+print(locations)
+
+
+print("Sort List:")
+locations.sort()
+print(locations)
+
+print("Sort List Reverse:")
+locations.sort(reverse= True)
+print(locations)

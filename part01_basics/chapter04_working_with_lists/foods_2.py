a25a5f95c62c48bf05262092ce8ff0cc0a07380c
khoa.hdinh <khoa.hdinh@gmail.com>
2026-07-18 15:27:27 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter04_working_with_lists/foods_2.py
@@ -0,0 +1,14 @@
+my_foods = ["banh xeo", "banh hoi", "banh beo", "banh canh"]
+
+
+# copyying a list must use Slice
+friend_foods = my_foods[:]
+
+my_foods.append("coconut")
+friend_foods.append("ice cream")
+
+print("My favorite food are:")
+print(my_foods)
+
+print("\nMy friend's favorite food are:")
+print(friend_foods)

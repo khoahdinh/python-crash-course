7fed4588084176e8980bd648e13a5a75c2cc9e79
khoa.hdinh <khoa.hdinh@gmail.com>
2026-07-18 10:41:43 +0700

    Added code files for entire book.

--- part01_basics/chapter04_working_with_lists/squares.py
+++ part01_basics/chapter04_working_with_lists/squares.py
@@ -1,7 +1,12 @@
-squares = []
+# squares = []
 
-for value in range(1, 11):
-    squares.append(value**2)
+# for value in range(1, 11):
+#     squares.append(value**2)
+
+
+# A list comprehension combines the for loop and the creation of new elements into one line
+# and automatically appends each new element
+squares = [value**2 for value in range(1, 11)]
 
 print(squares)
 

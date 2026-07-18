db20de9cb44fcf8d7d5edda0a8bc4358a28f6fb0
khoa.hdinh <khoa.hdinh@gmail.com>
2026-07-18 16:58:51 +0700

    Added code files for entire book.

--- part01_basics/chapter04_working_with_lists/buffet.py
+++ part01_basics/chapter04_working_with_lists/buffet.py
@@ -1,11 +1,9 @@
 buffet = ("rice", "melon", "apple", "fish", "pork")
-
 for dish in buffet:
     print(dish)
 
 # rewrites the tuples
 print("New menu:")
 buffet = ("coca", "melon", "corn", "fish", "pork")
-
 for dish in buffet:
     print(dish)

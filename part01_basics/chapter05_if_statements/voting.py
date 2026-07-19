dfce8f7131122438df103142685d618ab1e30b0f
khoa.hdinh <khoa.hdinh@gmail.com>
2026-07-19 09:58:21 +0700

    Added code files for entire book.

--- part01_basics/chapter05_if_statements/voting.py
+++ part01_basics/chapter05_if_statements/voting.py
@@ -1,5 +1,8 @@
-age = 18
+age = 17
 
 if age >= 18:
     print("You are old enough to vote.")
     print("Have you registered to vote yet?")
+else:
+    print("Sorry bro. You are too young to vote.")
+    print("Please eat more.")

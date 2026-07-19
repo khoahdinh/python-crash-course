c61c124661a7cf7826e9e3987268d3335c59c073
khoa.hdinh <khoa.hdinh@gmail.com>
2026-07-19 10:44:04 +0700

    Added code files for entire book.

--- part01_basics/chapter05_if_statements/toppings.py
+++ part01_basics/chapter05_if_statements/toppings.py
@@ -2,3 +2,14 @@ requested_topping = "mushrooms"
 
 if requested_topping != "anchoives":
     print("Hold the Anchoives!")
+
+requested_toppings = ["mushrooms", "extra cheese"]
+
+if "mushrooms" in requested_toppings:
+    print("Adding mushrooms.")
+if "pepper" in requested_toppings:
+    print("Adding pepper.")
+if "extra cheese" in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")

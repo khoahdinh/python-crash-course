eb97986592f7b3c663ee00a362fe4ad7f420ca96
khoa.hdinh <khoa.hdinh@gmail.com>
2026-07-19 09:36:28 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter05_if_statements/banned_users.py
@@ -0,0 +1,5 @@
+banned_users = ["khoa", "dung", "bo"]
+user = "nhi"
+
+if user not in banned_users:
+    print(user.title() + ", you are free to go!")
--- part01_basics/chapter05_if_statements/cars.py
+++ part01_basics/chapter05_if_statements/cars.py
@@ -5,3 +5,14 @@ for car in cars:
         print(car.upper())
     else:
         print(car.title())
+
+car = "subaru"
+print("Is car == 'subaru'? I predict True.")
+print(car == "subaru")
+
+print("\nIs car == 'huyndai'? I predict False.")
+print(car == "huyndai")
+
+car = "yamaha"
+print("\nIs car == 'yamaha'? I predict True.")
+print(car == "yamaha")
